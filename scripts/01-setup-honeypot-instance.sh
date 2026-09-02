3e7f04d67b9dbb975f6b90c2c1359c5a44b1e369
10ve-jaafar <jaafar.lamnayi@gmail.com>
2026-09-02 13:17:36 +0100

    Remove comments and streamline setup script

--- scripts/01-setup-honeypot-instance.sh
+++ scripts/01-setup-honeypot-instance.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# À exécuter sur l'instance Honeypot (10.0.1.215)
-# Préparation système et sécurisation de l'accès SSH admin
-
 set -e
 
 sudo apt-get update && sudo apt-get upgrade -y
+
 sudo apt-get install -y git python3-virtualenv python3-dev \
   libssl-dev libffi-dev build-essential libpython3-dev \
   python3-minimal authbind virtualenv
+
 sudo adduser --disabled-password cowrie
+
 sudo sed -i 's/#Port 22/Port 4422/' /etc/ssh/sshd_config
 sudo systemctl restart sshd
-
