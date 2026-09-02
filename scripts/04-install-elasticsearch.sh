3fcf16c622ef11605521cc128c55eb08abb8cbc1
10ve-jaafar <jaafar.lamnayi@gmail.com>
2026-09-02 13:20:35 +0100

    Fix script to ensure Elasticsearch starts correctly

--- scripts/04-install-elasticsearch.sh
+++ scripts/04-install-elasticsearch.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# À exécuter sur elk_server (10.0.2.10)
-
 set -e
 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | \
   sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
-| \
 
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] \
+  https://artifacts.elastic.co/packages/8.x/apt stable main" | \
   sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 
 sudo apt-get update
@@ -15,5 +14,3 @@ sudo apt-get install -y elasticsearch
 sudo systemctl daemon-reload
 sudo systemctl enable elasticsearch
 sudo systemctl start elasticsearch
-
-
