919c3a77ff38a4400f7458acef8b33df9a9f7c64
10ve-jaafar <jaafar.lamnayi@gmail.com>
2026-09-02 13:18:48 +0100

    Update 02-install-cowrie.sh

--- scripts/02-install-cowrie.sh
+++ scripts/02-install-cowrie.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# À exécuter en tant qu'utilisateur "cowrie" sur l'instance Honeypot
-# sudo su - cowrie
-# bash 02-install-cowrie.sh
-
 set -e
 
 git clone https://github.com/cowrie/cowrie.git
@@ -22,4 +18,3 @@ sudo chown cowrie /etc/authbind/byport/22
 sudo chown cowrie /etc/authbind/byport/23
 sudo chmod 755 /etc/authbind/byport/22
 sudo chmod 755 /etc/authbind/byport/23
-
